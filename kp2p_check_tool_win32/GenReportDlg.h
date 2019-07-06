a94269f18e40853725031f4b3d5eea958df5079a
tuyungang <yungangtu@e-seenet.com>
2019-07-06 18:41:18 +0800

    修改了界面，增加了保存，查看，按日期，类型检索控件，后续增加相应的功能逻辑

--- kp2p_check_tool_win32/GenReportDlg.h
+++ kp2p_check_tool_win32/GenReportDlg.h
@@ -20,12 +20,31 @@ public:
 
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+	virtual BOOL OnInitDialog();
 
 	DECLARE_MESSAGE_MAP()
 
 public:
 	Ckp2p_check_tool_win32Dlg *m_Parent;
 
+	CString       m_SavePahtEdit;
+	CDateTimeCtrl m_ChooseDateTimeCtrl;
+	CComboBox     m_ChooseDataTypeComboBox;
+	CListBox      m_UnChooseItemListBox;
+	CListBox      m_ChoosedItemListBox;
+
+	BOOL          m_bOnceAgain;
+
 public:
 	void SetParent(PVOID arg);
+	afx_msg void OnBnClickedAddItemSaveButton();
+	afx_msg void OnBnClickedDelItemSaveButton();
+	afx_msg void OnBnClickedClearItemSaveButton();
+	afx_msg void OnBnClickedBrowseFileSaveButton();
+	afx_msg void OnBnClickedSaveFileSaveButton();
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	afx_msg void OnBnClickedLoadDevIdSaveButton();
+	afx_msg void OnBnClickedCheckItemSaveButton();
+	afx_msg void OnBnClickedAddAllItemSaveButton();
 };
